b859a837b144386d3ed016c6f0bcac28d81de6c4
cx19105 <cx19105@bristol.ac.uk>
2022-03-10 17:59:27 +0000

    Fixed Bugs

--- mapJS.js
+++ mapJS.js
@@ -6,7 +6,7 @@ var map = L.map("map", {
     zoom: 15,
     maxZoom: 15});
 
-$.getJSON('http://geoportal1-ons.opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.geojson?outSR={%22latestWkid%22:27700,%22wkid%22:27700}').then(function(geoJSON) {
+$.getJSON('https://geoportal1-ons.opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.geojson?outSR={%22latestWkid%22:27700,%22wkid%22:27700}').then(function(geoJSON) {
   var osm = new L.TileLayer.BoundaryCanvas("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     boundary: geoJSON,
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, UK shape <a href="https://github.com/johan/world.geo.json">johan/word.geo.json</a>'
