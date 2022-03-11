5423b0685f1cc33cc593bd68fbb481ed68a695b7
sam.ward <sam.ward@starhat.co.uk>
2022-03-11 10:34:06 +0000

    Added API query examples

--- api_query_examples.py
+++ api_query_examples.py
@@ -1,4 +1,14 @@
-from api_requests import *
+import overpy
+
+
+def get_query_response(query_str):
+    """
+    :param query_str: String representing Overpass query
+    :return: JSON response
+    """
+    api = overpy.Overpass()
+    response = api.query(query_str)
+    return response
 
 
 def uk_pubs():
