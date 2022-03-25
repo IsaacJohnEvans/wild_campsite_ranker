87595c03870424e61a634a4d40d29e84c2afd907
cx19105 <cx19105@bristol.ac.uk>
2022-03-25 10:44:01 +0000

    Added zoom level

--- map.py
+++ map.py
@@ -13,12 +13,14 @@ def home():
 
 @app.route('/result',methods=['POST', 'GET'])
 def result():
-    output = request.form['mouse_info']
-    print("Output" + output, flush=True)
+    mouse_pos = request.form['mouse_info']
+    zoom_level = request.form['zoom_level']
+    print("Output :" + mouse_pos, flush=True)
+    print("Zoom level :" + zoom_level, flush=True)
 
     return render_template('bivouac.html')
     
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
