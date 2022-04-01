54877c6a9f5215c6dbbc1eaec10999b723d40a11
cx19105 <cx19105@bristol.ac.uk>
2022-04-01 20:51:23 +0100

    Preferences now saved as list instead of string

--- map.py
+++ map.py
@@ -8,7 +8,7 @@ from OSGridConverter import latlong2grid
 
 class Optimiser():
     def __init__(self, latlon, zoom_level, bbox, features, preferences):
-        self.preferences = preferences
+        self.preferences = self.updatePreferences(preferences)
         self.latlon = latlon
         self.zoom_level = zoom_level
         self.bbox = self.getBBoxList(bbox)
@@ -38,6 +38,13 @@ class Optimiser():
         tempWind = get_weather['features'][0]['properties']
         return tempWind
 
+    def updatePreferences(self, newPreferences):
+        preferences = []
+        for i in newPreferences:
+            if i.isdigit():
+                preferences.append(i)
+        return preferences
+
     def printStats(self):
         print("Latlon:",self.latlon, flush=True)
         print("zoom_level:",self.zoom_level, flush=True)
@@ -57,7 +64,8 @@ def get_preferences():
         preferences = request.form['preferences']
         data = {'status':"success"}
         try:
-            optimiser.preferences = preferences
+            optimiser.preferences = optimiser.updatePreferences(preferences)
+            print(optimiser.preferences, flush=True)
         except NameError:
             pass
         #print(preferences, flush=True)
@@ -72,7 +80,6 @@ def process_result():
         zoom_level = request.form['zoom_level']
         bbox = request.form['bbox']
         preferences = request.form['vals']
-
         features = request.form['features']
         # print(features)
         with open('data.geojson', 'w') as f:
@@ -85,7 +92,6 @@ def process_result():
         # print("Output :" + mouse_pos, flush=True)
         # print("Zoom level :" + zoom_level, flush=True)
         # print("Features :" + features, flush=True)
-
         # Define loads of interesting things here, ie list of coords to plot that is a path
         num_features = get_num_features(features)
 
